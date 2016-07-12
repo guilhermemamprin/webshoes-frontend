175e9589acb738fc4f215dce12057a4490f313b6
Guilherme Mamprin <guilherme.mamprin@gmail.com>
2016-07-12 11:09:16 -0300

    sign up and login integrations implemented.

--- src/app/controllers/user/user.controller.ts
+++ src/app/controllers/user/user.controller.ts
@@ -7,32 +7,56 @@ module webShoes {
     phone    : string;
     cpf      : string;
     rg       : string;
+    email    : string;
 
   }
 
   export class UserController {
     public $http: any;
     public $window: any;
+    private $state: any;
     public rootUrl: string = 'http://webshoes-backend.herokuapp.com';
     public user: User;
+    public userLogin: User;
 
-
-
-      constructor ($scope: any, $http: any, $window: any) {
+      constructor ($scope: any, $http: any, $window: any, $state:any) {
         this.$http = $http;
         this.$window = $window;
+        this.$state = $state;
       }
 
-      onSignUp() {
+      onSignUp(): void {
 
         this.$http({
             method  : 'POST',
             url     :  this.rootUrl + '/signup',
             data    :  this.user
-          }).then(function(response) {
+          }).then((response) => {
             this.$window.localStorage.setItem('username', response.data.login);
-            this.$window.location.href = '/';
+            alert('Usuário cadastrado!');
+            this.userLogin = this.user;
+            this.onSignIn();
+
+          }, (errorResponse) => {
+            alert('Erro: ' + errorResponse.message);
+        });
+    }
+
+    onSignIn(): void {
+      this.$http({
+            method  : 'POST',
+            url     :  this.rootUrl + '/signin',
+            data    :  {email: this.userLogin.email,  password: this.userLogin.password}
+          }).then((response) => {
+
+            this.$window.localStorage.setItem('name', response.data.name);
+            this.$window.localStorage.setItem('token', response.data.token);
+            this.$state.go('home');
+        }, (errorResponse) => {
+            alert('Erro: ' + errorResponse.message);
         });
     }
   }
 }
+
+
