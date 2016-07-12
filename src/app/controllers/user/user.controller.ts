89113b4524823f3e335f2411c344d3421f03f207
Guilherme Mamprin <guilherme.mamprin@gmail.com>
2016-07-11 23:34:26 -0300

    implemented signup

--- src/app/controllers/user/user.controller.ts
+++ src/app/controllers/user/user.controller.ts
@@ -1,10 +1,38 @@
 module webShoes {
+  'use strict';
 
-	export class UserController {
-		public $http: any;
+  class User {
+    name     : string;
+    password : string;
+    phone    : string;
+    cpf      : string;
+    rg       : string;
 
-	    constructor ($scope, $http) {
-	      this.$http = $http;
-	    }
-	}
+  }
+
+  export class UserController {
+    public $http: any;
+    public $window: any;
+    public rootUrl: string = 'http://webshoes-backend.herokuapp.com';
+    public user: User;
+
+
+
+      constructor ($scope: any, $http: any, $window: any) {
+        this.$http = $http;
+        this.$window = $window;
+      }
+
+      onSignUp() {
+
+        this.$http({
+            method  : 'POST',
+            url     :  this.rootUrl + '/signup',
+            data    :  this.user
+          }).then(function(response) {
+            this.$window.localStorage.setItem('username', response.data.login);
+            this.$window.location.href = '/';
+        });
+    }
+  }
 }
