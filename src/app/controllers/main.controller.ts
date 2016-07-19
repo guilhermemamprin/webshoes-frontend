2a29ba12dff03a30d0d78d8274703fb6c3a37ee9
Lucas Batista Gabriel <lucas.bgabriel@gmail.com>
2016-07-19 10:40:46 -0300

    change main controller to get product list

--- src/app/controllers/main.controller.ts
+++ src/app/controllers/main.controller.ts
@@ -1,79 +1,41 @@
 module webShoes {
   'use strict';
 
-  class Thing {
-    public rank: number;
-    public login: string;
-    public avatar_url: string;
-    public html_url: string;
-
-    constructor(login: string, avatar_url: string, rank: number, html_url: string) {
-      this.login = login;
-      this.avatar_url = avatar_url;
-      this.html_url = html_url;
-
-    }
+  class Product {
+    id         : number;
+    img1       : string;
+    img2       : string;
+    img3       : string;
+    img4       : string;
+    manufactor : string;
+    name       : string;
+    price      : number;
+    quantity   : number;
+    size       : string;
   }
 
   export class MainController {
-    public awesomeThings: Thing[];
-    public search: string;
-    private pendingTask: any;
-    private $http: any;
+    public $http: any;
+    public $window: any;
+    public rootUrl: string = 'http://webshoes-backend.herokuapp.com';
+    public productList : Array<Product>;
 
-    updateGrid(response) {
-      var _this = this;
-        this.awesomeThings = new Array<Thing>();
-        _.forEach(response.data, function(awesomeThing: Thing) {
-            _this.awesomeThings.push(awesomeThing);
-        });
-    }
-
-    getAllUsers() {
-        var _this = this;
-        var data;
-        this.$http.get('https://api.github.com/users')
-            .then(function(response){_this.updateGrid(response);});
-    }
-
-    fetchUser() {
-        var awesomeThings;
-        var _this = this;
-        if (this.search.length <= 0) {
-            this.getAllUsers();
-            return;
-        }
-        this.$http.get('https://api.github.com/search/users?q=' + this.search)
-            .then(function(response) {
-                awesomeThings = response.data.items;
-                _this.awesomeThings = new Array<Thing>();
-
-                awesomeThings.forEach(function(profile: Thing) {
-                  _this.awesomeThings.push(profile);
-
-                });
-        });
-    }
-
-    change() {
-      if (this.pendingTask) {
-        clearTimeout(this.pendingTask);
+      constructor ($scope: any, $http: any, $window: any) {
+        this.$http = $http;
+        this.$window = $window;
+        this.onGetProducList();
       }
-      this.pendingTask = setTimeout(this.fetchUser(), 1000);
-    }
 
+      onGetProducList() : void {
 
-    /* @ngInject */
-    constructor ($scope, $http) {
-      var vm = this;
-      var awesomeThings = [];
-      var pendingTask;
-
-      this.$http = $http;
-
-      if(this.search === undefined){
-        this.getAllUsers();
-      }
+        this.$http({
+            method  : 'GET',
+            url     :  this.rootUrl + '/products',
+          }).then((response : any) => {
+            this.productList = <Array<Product>>_.get(response, 'data.products');
+        }, (errorResponse) => {
+          alert('Erro: ' + errorResponse.message);
+        });
     }
   }
 
